a8859dcf5e404dd214859cac6a7a1fbc306b9180
Calvin Cheng <calvincheng2000@gmail.com>
2021-06-23 16:26:16 -0400

    feat: added custom bottom nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import {View, Image, TouchableOpacity} from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -12,17 +13,61 @@ import ColorSet from './frontend/resources/themes/Global'
 
 const Tab = createBottomTabNavigator();
 
+const CustomTabBarButton = ({children, onPress}) => (
+    <TouchableOpacity style={{top:-30, width:70, height:70, justifyContent:"center", alignItems:"center"}} onPress={onPress}>
+        <View style={{ backgroundColor:"#9CC69B", width:70, height:70,borderRadius:35}}>
+            {children}
+        </View>
+    </TouchableOpacity>)
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator  initialRouteName="TabOne" backBehavior="order" tabBarOptions={{
-            activeTintColor: ColorSet.examplePrimary}}>
-        <Tab.Screen name="TabOne" component={TabOne} options={{
-          tabBarLabel: 'TabOne',
-          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}/>
-        <Tab.Screen name="TabTwo" component={TabTwo} />
-        <Tab.Screen name="TabThree" component={TabThree} />
-        </Tab.Navigator>    
+          activeTintColor: ColorSet.examplePrimary,
+          inactiveTintColor: "#505050",
+          style: { backgroundColor: '#BDE4A8'}
+
+      }}>
+        <Tab.Screen name="Calender" component={TabOne} options={{
+          tabBarLabel: 'Calender',
+          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="calendar" color={color} size={size} />)}}/>
+          <Tab.Screen name="Tab" component={TabThree}
+                      options={{
+                          tabBarLabel: 'Temp1',
+                          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}
+          />
+        <Tab.Screen name={'TabMiddle'} component={TabTwo}
+                    options={{
+                        tabBarLabel:() => {return null},
+                        tabBarIcon: ({focused}) => (
+                            <Image
+                            source={require("./frontend/resources/assets/plus-sign.png")}
+                            resizeMode="contain"
+                            style={{
+                                width:35,
+                                height:35,
+                                tintColor:"#505050"
+                            }}
+                            />
+                        ),
+                        tabBarButton: (props) => (
+                            <CustomTabBarButton {... props}/>
+                        ),
+                    }}
+
+        />
+          <Tab.Screen name="TabFour" component={TabThree}
+                      options={{
+                          tabBarLabel: 'Temp2',
+                          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}
+          />
+        <Tab.Screen name="TabThree" component={TabThree}
+                    options={{
+                        tabBarLabel: 'Habit',
+                        tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="clipboard-check" color={color} size={size} />)}}
+        />
+        </Tab.Navigator>
     </NavigationContainer>
   );
 }
