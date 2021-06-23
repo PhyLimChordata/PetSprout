ab449883cf588e84e0577642f4aae9ead8b905ba
Yuanyuan Li <tuanzi.li@mail.utoronto.ca>
2021-06-24 06:27:04 +0800

    add middleware-authentication, function folder

--- src/backend/routes/userRoute.js
+++ src/backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-let User = require('../models/UserSchema')
+let User = require('../schemas/UserSchema')
 const {check, validationResult} = require("express-validator")
 const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
