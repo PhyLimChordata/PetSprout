3362e8129c5a7c4ec158ab358c6c70da86a6a433
Yuanyuan Li <tuanzi.li@mail.utoronto.ca>
2021-06-24 04:37:56 +0800

    adding login route

--- src/backend/routes/userRoute.js
+++ src/backend/routes/userRoute.js
@@ -9,7 +9,7 @@ router.post('/register',
 [
     check('userName','Name is empty').not().isEmpty(),
     check('email','E-mail is empty').isEmail(),
-    check('password','Passwords needs to contains 6 letter and less than 10').isLength({min:6,max:12})
+    check('password','Passwords needs to contains 6 letter and less than 12').isLength({min:6,max:12})
 ],
 async(req,res)=>{
     try{
@@ -36,12 +36,14 @@ async(req,res)=>{
             password
         })
 
-        // hashedpassword
+        // hashedpassword (security)
         const salt = await bcryptjs.genSalt(10)
         let hashedPassword = await bcryptjs.hash(password,salt)
         newUser.password = hashedPassword
 
+        //save to the database
         await newUser.save();
+
         const payload={
             user:{
                 id:newUser._id
@@ -57,6 +59,7 @@ async(req,res)=>{
                 res.json({token})
             }
         )
+
         res.send('User is created')
     }
     catch(error){
@@ -65,5 +68,49 @@ async(req,res)=>{
     }
 })
 
+router.post("/login", 
+[
+    check('email','E-mail is empty').isEmail(),
+],
+async (req,res) => {
+    try {
+        let{email,password} = req.body
+        let errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()})
+        }
+
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(404).send("User has not been created yet")
+        }
+
+        let matching=await bcryptjs.compare(password,user.password)
+        if(!matching){
+            return res.status(401).send("password doesn't match")
+        }
+
+        const payload = {
+            user: {
+                id: user._id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jsonWebTokenSecret'),
+            {expiresIn: 3600},
+            (err,token) =>{
+                if(err) throw err
+                res.json({token})
+            }
+        )   
+
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send("server error")
+    }
+})
 
 module.exports = router
