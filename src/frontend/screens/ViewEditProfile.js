8611fcfa0eee11e7d915614c5fc7e87cba73101e
Cheryl <cheryl.pl.chen@hotmail.com>
2021-06-28 13:22:45 +0800

    Added js and css files for the view and edit profile screens

---
+++ src/frontend/screens/ViewEditProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ProfileEdit(props){
+    return({
+
+    });
+}
+
+function displayEdit(props){
+    return({
+
+    });
+}
+
+
+export default ProfileEdit
---
+++ src/frontend/styling/ViewEditBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ColorSet from '../resources/themes/Global';
+
+const viewEditBox = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: column,
+        textAlign: "left",
+        backgroundColor: ColorSet.PrimaryGreen,
+        
+    },
+
+})
