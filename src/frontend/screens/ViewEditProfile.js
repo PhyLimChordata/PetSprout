1287c9130dc9cb17a6e629136d4b6cfcc5348df6
Cheryl <cheryl.pl.chen@hotmail.com>
2021-06-28 20:17:57 +0800

    Added edit boxes and styling

--- src/frontend/screens/ViewEditProfile.js
+++ src/frontend/screens/ViewEditProfile.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import ColorSet from '../resources/themes/Global.js';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { Typography } from '@material-ui/core';
+import { ThemeProvider } from '@react-navigation/native';
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: 'Roboto',
+    },
+    palette: {
+        primary: {
+            main: ColorSet.PrimaryGreen,
+        },
+        secondary: {
+            main: ColorSet.SecondaryGreen,
+        },
+        background: {
+            main: 'white',
+            dark: Colors.BackgroundGrey,
+        }
+    }
+});
 
 function ProfileEdit(props){
-    return({
-
-    });
+    return(
+        <View>
+           <EditBox tag="Username" defaultValue="My username"/> 
+        </View>
+        
+    );
 }
 
-function displayEdit(props){
-    return({
-
-    });
-}
-
+const EditBox = (props) => {
+    return(
+        <ThemeProvider theme={theme}>
+            <Typography>{props.tag}</Typography>
+            <TextField
+                variant="standard"
+                size="small"
+                defaultValue={props.defaultValue}
+                {...props.multiline}
+                />
+        </ThemeProvider>
+    );
+};
 
 export default ProfileEdit
