73daddbe902ae884447089c9e206ed50b20f893a
Calvin Cheng <42909672+calvin7889@users.noreply.github.com>
2021-07-01 12:16:55 -0400

    feat: added custom bottom nav bar (#2)
    * feat: added custom bottom nav bar
    
    * fix: moved colors to resources, made button not change opacity on hold
    
    * feat: middle button now rotates on click
    
    * chore: changed color of bottom nav abr
    
    * Speed up spinning on add bar
    
    Co-authored-by: Andy Phy Lim <Andyphyl00@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import {View, TouchableOpacity, Animated} from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -12,17 +13,85 @@ import ColorSet from './frontend/resources/themes/Global'
 
 const Tab = createBottomTabNavigator();
 
+const CustomTabBarButton = ({children, onPress}) => (
+    <TouchableOpacity activeOpacity={1} style={{top:-30, width:70, height:70, justifyContent:"center", alignItems:"center"}} onPress={onPress}>
+        <View style={{ backgroundColor:ColorSet.QuaternaryGreen, width:70, height:70,borderRadius:35}}>
+            {children}
+        </View>
+    </TouchableOpacity>)
+
+
+const animation = new Animated.Value(0);
+
+
+const rotate = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+});
+
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator  initialRouteName="TabOne" backBehavior="order" tabBarOptions={{
-            activeTintColor: ColorSet.examplePrimary}}>
-        <Tab.Screen name="TabOne" component={TabOne} options={{
-          tabBarLabel: 'TabOne',
-          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}/>
-        <Tab.Screen name="TabTwo" component={TabTwo} />
-        <Tab.Screen name="TabThree" component={TabThree} />
-        </Tab.Navigator>    
+          activeTintColor: ColorSet.QuinaryGreen,
+          inactiveTintColor: ColorSet.white,
+          style: { backgroundColor: ColorSet.TertiaryGreen}
+
+      }}>
+        <Tab.Screen name="Calendar" component={TabOne} options={{
+          tabBarLabel: 'Calendar',
+          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="calendar" color={color} size={size} />)}}/>
+          <Tab.Screen name="Tab" component={TabThree}
+                      options={{
+                          tabBarLabel: 'Temp1',
+                          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}
+          />
+        <Tab.Screen name={'TabMiddle'} component={TabTwo}
+                    listeners={{
+                        tabPress: e => {
+                            // rotate
+                            Animated.timing(animation, {
+                                toValue: 1,
+                                duration: 275,
+                                useNativeDriver: false,
+                            }).start(() => {
+                                animation.setValue(0);
+                            });
+                            // Prevent default action
+                            e.preventDefault();
+                        }
+                    }}
+                    options={{
+                        tabBarLabel:() => {return null},
+                        tabBarIcon: ({focused}) => (
+                            <Animated.Image
+                            source={require("./frontend/resources/assets/plus-sign.png")}
+                            resizeMode="contain"
+                            style={{
+                                width:35,
+                                height:35,
+                                tintColor: ColorSet.white,
+                                transform: [ { rotate: rotate }]
+                            }}
+                            />
+                        ),
+                        tabBarButton: (props) => (
+                            <CustomTabBarButton {... props}/>
+                        ),
+                    }}
+
+        />
+          <Tab.Screen name="TabFour" component={TabThree}
+                      options={{
+                          tabBarLabel: 'Temp2',
+                          tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />)}}
+          />
+        <Tab.Screen name="TabThree" component={TabThree}
+                    options={{
+                        tabBarLabel: 'Habit',
+                        tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="clipboard-check" color={color} size={size} />)}}
+        />
+        </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- src/frontend/resources/themes/Global.js
+++ src/frontend/resources/themes/Global.js
@@ -1,6 +1,13 @@
 const ColorSet = {
     examplePrimary: '#FF5733',
     white: '#ffffff',
+// <<<<<<< HEAD
+//     someColor: '#EF2723',
+//     primaryColor: '#D7F2BA',
+//     secondaryColor: '#BDE4A8',
+//     tertiaryColor:'#9CC69B',
+//     quaternaryColor: '#6E8F6D'
+// =======
     grey: '#B7BEB0',
     someColor: '#EF2723',
     PrimaryGreen: '#F5FEEC',
@@ -9,6 +16,7 @@ const ColorSet = {
     QuaternaryGreen: '#9CC69B',
     QuinaryGreen: '#6E8F6D',
     BackgroundGrey: '#505050'
+// >>>>>>> development
 }
 
 export default ColorSet;
