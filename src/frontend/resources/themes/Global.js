de931f2ef5c4c953ff87fe20b7180502ffa3b07b
Andy Phy Lim <Andyphyl00@gmail.com>
2021-07-24 09:33:40 -0400

    Remove comments

--- src/frontend/resources/themes/Global.js
+++ src/frontend/resources/themes/Global.js
@@ -1,13 +1,6 @@
 const ColorSet = {
     examplePrimary: '#FF5733',
     white: '#ffffff',
-// <<<<<<< HEAD
-//     someColor: '#EF2723',
-//     primaryColor: '#D7F2BA',
-//     secondaryColor: '#BDE4A8',
-//     tertiaryColor:'#9CC69B',
-//     quaternaryColor: '#6E8F6D'
-// =======
     grey: '#B7BEB0',
     someColor: '#EF2723',
     PrimaryGreen: '#F5FEEC',
@@ -16,7 +9,6 @@ const ColorSet = {
     QuaternaryGreen: '#9CC69B',
     QuinaryGreen: '#6E8F6D',
     BackgroundGrey: '#505050'
-// >>>>>>> development
 }
 
 export default ColorSet;
