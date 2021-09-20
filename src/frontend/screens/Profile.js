94e9450e599ab21acd2c55e0f3498e3019ba2622
Cheryl <cheryl.pl.chen@hotmail.com>
2021-09-20 23:38:51 +0800

    Username field cannot be edited. Fixed popup for when information has been updated successfully.

--- src/frontend/screens/Profile.js
+++ src/frontend/screens/Profile.js
@@ -6,20 +6,15 @@ import {
 	Dimensions,
 	TextInput,
 	SafeAreaView,
+	Image,
+	ImageBackground
 } from 'react-native';
-// import profileStyles from '../styling/Profile';
-import StyleSheetFactory from '../styling/ViewEditBox';
 import profileStyles from '../styling/Profile';
 import MenuHeader from '../components/MenuHeader';
-import { useFonts, Roboto_900Black } from '@expo-google-fonts/roboto';
 import { AuthContext } from '../Context';
 import { useTheme } from '@react-navigation/native';
 import BottomPopup from '../components/BottomPopup';
-
-// let styles = StyleSheetFactory.getSheet(
-// 	Dimensions.get('screen').width,
-// 	Dimensions.get('screen').height
-// );
+import Colours from '../resources/themes/Colours';
 
 function ProfileEdit(props) {
 	let popup = React.useRef();
@@ -29,7 +24,9 @@ function ProfileEdit(props) {
 	const[about, setAbout] = React.useState('');
 	const[createdAt, setCreatedAt] = React.useState('');
 	const[error, setError] = React.useState('')
+	const[message, setMessage] = React.useState('')
 	const[userFocused, setFocused] = React.useState(false)
+	const[color, setColor] = React.useState(Colours.Red.Error)
 
 	const { getToken } = useContext(AuthContext);
 	const { colors } = useTheme();
@@ -64,12 +61,10 @@ function ProfileEdit(props) {
 		if(userName == 'default') get();
 	}, []);
 
-	useEffect(() => console.log("Error = " + error), [error]);
-
 	const onSubmit = () => {
 		if(userName.length == 0) {
-			setError("Username cannot be empty.")
-			popup.current?.togglePopup();
+			//setError("Username cannot be empty.")
+			//popup.current?.togglePopup();
 		} else {
 			fetch('http://localhost:5000/api/v1.0.0/user/modifyAccount', {
 				method: 'PUT',
@@ -84,16 +79,22 @@ function ProfileEdit(props) {
 			})
 			.then((res) => {
 				if (res.status != 200) {
-					popup.current?.togglePopup();
-					if(res.status == 500){
-						setError('Something wrong happened internally...');
-					}
-				} 
-				res.json().then((data) => {
-					console.log(data)
-					setUserName(data.userName)
-					setAbout(data.about)
-				});
+					setMessage("The provided information cannot be saved");
+					setColor(Colours.Red.Error)
+				} else {
+					console.log("Success")
+					setMessage("Account has been successfully updated.")
+					setColor(Colours.Green.Success)
+					res.json().then((data) => {
+						console.log(data)
+						setUserName(data.userName)
+						setAbout(data.about)
+					});
+				}
+			})
+			.then(() => {
+				console.log(message);
+				popup.current?.togglePopup();
 			})
 			.catch(err => console.log(err))
 		}
@@ -117,81 +118,99 @@ function ProfileEdit(props) {
 
 	return (
 		<>
-		<SafeAreaView>
-			<View>
-				<MenuHeader text='Account  ' navigation={props.navigation}>
-					<Text>Account </Text>
-				</MenuHeader>
-			</View>
-			<View style={styles.container}>
-				<View style={styles.formContainer}>
-					<Text
-						style={[
-							styles.textTitle,
-							styles.text,
-							{ fontWeight: '900' },
-							error.length != 0 ? styles.textInputError : null
-						]}>
-						Username
-					</Text>
-					<TextInput
-						onFocus={() => setFocused(true)}
-						onBlur={() => setFocused(false)}
-						onChangeText={(text) => setUserName(text)}
-						value={userName}
-						style={[
-								styles.textInput,
+			<SafeAreaView>
+				<View>
+					<MenuHeader text='Account  ' navigation={props.navigation}
+						right={
+							<>
+								<ImageBackground
+									style={{
+										width: Dimensions.get('screen').width * 0.1,
+										height: Dimensions.get('screen').width * 0.1,
+										alignItems: 'center',
+										justifyContent: 'center'
+									}}
+									source={require('../resources/images/Background.png')}
+								>
+									<Image
+										style={{
+											width: Dimensions.get('screen').width * 0.08,
+											height: Dimensions.get('screen').width * 0.08,
+											margin: Dimensions.get('screen').width * 0.02
+										}}
+										source={require('../resources/images/Account.png')}
+									/>
+								</ImageBackground>
+							</>
+						}/>
+				</View>
+				<View style={styles.container}>
+					<View style={styles.formContainer}>
+						<Text
+							style={[
+								styles.textTitle,
 								styles.text,
-								userFocused ? styles.textInputSelected : null,
-								error.length != 0 ? styles.textInputError : null
-							]}
-					/>
+								{ fontWeight: '900' },
+							]}>
+							Username
+						</Text>
+						<TextInput
+							onFocus={() => setFocused(true)}
+							onBlur={() => setFocused(false)}
+							value={userName}
+							style={[
+									styles.textInput,
+									styles.text,
+									userFocused ? styles.textInputSelected : null,
+								]}
+						/>
+					</View>
+					<View style={styles.formContainer}>
+						<Text
+							style={[
+								styles.textTitle,
+								styles.text,
+								{ fontWeight: '900' },
+							]}>
+							Email
+						</Text>
+						<Text style={[styles.textInput, styles.text]}>{email}</Text>
+					</View>
+					<View style={styles.formContainer}>
+						<Text
+							style={[
+								styles.textTitle,
+								styles.text,
+								{ fontWeight: '900' },
+							]}>
+							About
+						</Text>
+						<TextInput
+							onChangeText={(text) => setAbout(text)}
+							value={about}
+							style={styles.textMultiInput}
+							multiline={true}
+							numberOfLines={5}
+						/>
+					</View>
+					<View style={styles.formContainer}>
+						<Text
+							style={[
+								styles.textTitle,
+								styles.text,
+								{ fontWeight: '900' },
+							]}>
+							Account Created On
+						</Text>
+						<Text style={[styles.textInput, styles.text]}>{createdAt}</Text>
+					</View>
+					<SubmitButton submit={onSubmit}/>
 				</View>
-				<View style={styles.formContainer}>
-					<Text
-						style={[
-							styles.textTitle,
-							styles.text,
-							{ fontWeight: '900' },
-						]}>
-						Email
-					</Text>
-					<Text style={[styles.textInput, styles.text]}>{email}</Text>
-				</View>
-				<View style={styles.formContainer}>
-					<Text
-						style={[
-							styles.textTitle,
-							styles.text,
-							{ fontWeight: '900' },
-						]}>
-						About
-					</Text>
-					<TextInput
-						onChangeText={(text) => setAbout(text)}
-						value={about}
-						style={styles.textMultiInput}
-						multiline={true}
-						numberOfLines={5}
-					/>
-				</View>
-				<View style={styles.formContainer}>
-					<Text
-						style={[
-							styles.textTitle,
-							styles.text,
-							{ fontWeight: '900' },
-						]}>
-						Account Created On
-					</Text>
-					<Text style={[styles.textInput, styles.text]}>{createdAt}</Text>
-				</View>
-				<SubmitButton submit={onSubmit}/>
-			</View>
-		</SafeAreaView>
+			</SafeAreaView>
 			<BottomPopup
 				ref={popup}
-				text={'The provided information cannot be saved'}
+				color={Colours.Green.Selected}
+				text={message}
 			/>
 		</>
 	);
