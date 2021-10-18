62a5136f80222c553e1efbb3741bfe6f352165ad
Cheryl <cheryl.pl.chen@hotmail.com>
2021-10-18 11:56:21 -0400

    Added achievements route

--- src/backend/schemas/achievementSchema.js
+++ src/backend/schemas/achievementSchema.js
@@ -9,21 +9,88 @@ const AchievementSchema = new Schema(
 		},
 		achievements: [
             {
-                category: {
-                    type: String,
-                },
-                subachievements: [
-                    {
-                       name: {
-                           type: String,
-                           default: ''
-                       } ,
-                       progress: {
-                           type: Number,
-                           default: 0
-                       }
+                streaks: {
+                    longest_streak:{
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
+                    },
+                    habits_with_streaks: {
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
                     }
-                ]
+                },
+                creature: {
+                    evolution_stages: {
+                        type: Number,
+                        default: 1
+                    },
+                    highest_level: {
+                        type: Number,
+                        default: 1
+                    },
+                    cosmetics_num: {
+                        type: Number,
+                        default: 1
+                    },
+                    pets_num :{
+                        type: Number,
+                        default: 1
+                    },
+                },
+                accountability: {
+                    days_alive: {
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
+                    },
+                    login_streak: {
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
+                    },
+                    friends_number: {
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
+                    },
+                    friends_helped: {
+                        value: {
+                            type: Number,
+                            default: 0
+                        },
+                        available: {
+                            type: Boolean,
+                            default: false
+                        }
+                    }
+                }
             }
         ]
     }
