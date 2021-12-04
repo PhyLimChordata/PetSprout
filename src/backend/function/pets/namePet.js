a2286676efc540e6c9b3690fc870b4843a6aa0d9
Andy Phy Lim <Andyphyl00@gmail.com>
2021-12-04 02:57:10 -0500

    feat: backend for pets

---
+++ src/backend/function/pets/namePet.js
@@ -0,0 +1,23 @@
+const Pets = require('../../schemas/petsSchema');
+const User = require('../../schemas/userSchema');
+
+const { validationResult } = require('express-validator');
+
+module.exports = async (req, res) => {
+    try {
+        let errors = validationResult(req);
+        if (!errors.isEmpty())
+			return res.status(400).json({ error: errors.array() });
+
+        let user = await User.findById(req.user.id).select('-password');
+        if (!user) return res.status(404).json('User could not found');
+
+        let currentPet = await Pets.findOne({user: req.user.id}).currentPet;
+        currentPet.name = req.name;
+        await currentPet.save();
+        res.json(currentPet);
+    } catch (error) {
+		console.error(error);
+		res.status(500).json('Server error');
+	}
+}
