73444f7588d999c36f0707e28f473dbe27ea48ff
Andy Phy Lim <Andyphyl00@gmail.com>
2021-12-05 15:36:57 -0500

    feat: replenish health on level

--- src/backend/function/habit/updateHabit.js
+++ src/backend/function/habit/updateHabit.js
@@ -39,8 +39,6 @@ module.exports = async (req, res) => {
 		) {
 			errors.push('schedule');
 		}
-		if (alarm.length === 0) errors.push('alarm');
-		if (times === 0) errors.push('times');
 		if (date === '') errors.push('date');
 
 		if (errors.length != 0) return res.status(403).json({ error: errors });
