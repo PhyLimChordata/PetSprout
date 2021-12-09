2223dbaa371c9e17f2f3a9298ede94cd972ca4ee
Calvin <calvincheng2000@gmail.com>
2021-12-08 23:45:20 -0500

    feat: evolution tree

---
+++ src/frontend/resources/mappings/EvolutionTreeMapping.js
@@ -0,0 +1,15 @@
+export const EvolutionTreeMapping = {
+    'egg': ['toeny', 'finny', 'wingin'],
+    'toeny': ['meerkat', 'turtle' , 'tapir' ],
+    'turtle': ['tortoise','armadillo' , 'pangolin'],
+    'meerkat': ['bear', 'red panda', 'kangaroo'],
+    'tapir': ['elephant', 'anteater', 'rhino'],
+    'wingin': ['na\' vi', 'bat', 'duck'],
+    'na\' vi': ['biblical angel', 'flying fish', 'butterfly'],
+    'bat': ['flying squirrel', 'wyvern', 'manta ray'],
+    'duck': ['phoenix', 'pegasus', 'griffin'],
+    'finny': ['snake', 'piranha', 'beta fish'],
+    'snake': ['hydra', 'seahorse', 'eel'],
+    'piranha': ['puffer fish', 'shark', 'angler fish'],
+    'beta fish': ['swordfish', 'lionfish',  'frilled lizard'],
+}
