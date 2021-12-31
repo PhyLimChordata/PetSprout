7d4da72b969919360d9007ed45bb6d253bc5fe51
Andy Phy Lim <Andyphyl00@gmail.com>
2021-12-31 05:37:08 -0500

    chore: comment sorting of habits to prevent grey box bug

--- src/frontend/screens/Habits.js
+++ src/frontend/screens/Habits.js
@@ -69,13 +69,13 @@ function HabitsScreen(props) {
 			.then((res) =>
 				res.json().then((data) => {
 					console.log(data.habitList);
-					data.habitList.sort(function (a, b) {
-						var keyA = a.times - a.todo;
-						var keyB = b.times - b.todo;
-						if (keyA < keyB) return 1;
-						if (keyA > keyB) return -1;
-						return 0;
-					});
+					// data.habitList.sort(function (a, b) {
+					// 	var keyA = a.times - a.todo;
+					// 	var keyB = b.times - b.todo;
+					// 	if (keyA < keyB) return 1;
+					// 	if (keyA > keyB) return -1;
+					// 	return 0;
+					// });
 					setTimeout(() => {
 						setHabits(data.habitList);
 						setUserHabitId(data._id);
@@ -159,7 +159,6 @@ function HabitsScreen(props) {
 								</Animated.View>
 							</View>
 						);
-						// 	}
 					})}
 				</Animated.ScrollView>
 			</View>
