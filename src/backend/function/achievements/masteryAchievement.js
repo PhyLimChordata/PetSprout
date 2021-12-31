276178a6e80b21443203486a041bddc371b6f78c
Cheryl <cheryl.pl.chen@hotmail.com>
2021-12-30 19:16:31 -0500

    Updated achievement database, added endpoints for mastery and habit count

---
+++ src/backend/function/achievements/masteryAchievement.js
@@ -0,0 +1,27 @@
+const Achievement = require('../../schemas/achievementSchema');
+const UserHabits = requre('../../schemas/habitSchema');
+
+const update_mastery_count = async(req, res) => {
+    try {
+        let user_habits = await UserHabits.findOne({ user: user_id });
+        if(!user_habits) return res.status(404).json('User habits not found');
+        const habitlist = user_habits.habitlist;
+
+        let mastery_count = 0;
+        for(let habit in habitlist) {
+            if(habit.continuous >= 66) mastery_count++;
+        }
+
+        let user_achievements = await Achievement.findOne({ user: user_id });
+        if(!user_achievements) return res.status(404).json('User achievements not found');
+        if(user_achievements.achievements.streaks.mastery < mastery_count) user_achievements.achievements.streaks.mastery = mastery_count;
+
+		await user_achievements.save();
+
+		res.status(200).json({ "mastery": mastery_count});
+
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json('server error');
+	}
+};
--- src/backend/schemas/achievementSchema.js
+++ src/backend/schemas/achievementSchema.js
@@ -1,56 +1,63 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const AchievementSchema = new Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-	},
-	achievements: 
-		{
-			streaks: {
-				commitment:{
-					type: Number,
-					default: 0
-				},
-				habit_count: {
-					type: Number,
-					default: 0
-				}
-			},
-			habipet: {
-				caretaker: {
-					type: Number,
-					default: 0
-				},
-				maturity: {
-					type: Number,
-					default: 0
-				},
-				pet_count :{
-					type: Number,
-					default: 0
-				},
-			},
-			accountability: {
-				health: {
-					type: Number,
-					default: 0
-				},
-				login: {
-					type: Number,
-					default: 0
-				},
-				friends_count: {
-					type: Number,
-					default: 0
-				},
-				supportive: {
-					type: Number,
-					default: 0
-				}
-			}
-		}
+const AchievementSchema = new Schema(
+	{
+        user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'user',
+		},
+		achievements: 
+            {
+                streaks: {
+                    commitment:{
+                        type: Number,
+                        default: 0
+                    },
+                    habit_count: {
+                        type: Number,
+                        default: 0
+                    },
+					mastery:{
+						type: Number,
+						default: 0
+					}
+                },
+                habipet: {
+                    caretaker: {
+                        type: Number,
+                        default: 0
+                    },
+                    maturity: {
+                        type: Number,
+                        default: 0
+                    },
+                    pet_count :{
+                        type: Number,
+                        default: 0
+                    },
+                },
+                accountability: {
+                    health: {
+                        type: Number,
+                        default: 0
+                    },
+                    login: {
+                        type: Number,
+                        default: 0
+                    },
+					// No friends feature 
+					/*
+                    friends_count: {
+                        type: Number,
+                        default: 0
+                    },
+                    supportive: {
+                        type: Number,
+                        default: 0
+                    }*/
+                }
+            }
     }
 );
 
