8cf21449569f0fc96b5c8c49dc09bbac9cc669e0
JoevinChen <joevinchen2003@gmail.com>
2022-01-13 22:03:23 -0500

    chore: fixed rogue style sheet

--- src/frontend/components/PomodoroTasks.js
+++ src/frontend/components/PomodoroTasks.js
@@ -1,70 +1,30 @@
 import React,{useState} from "react";
 import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput} from 'react-native';
-
+import PomodoroTasksStyles from "../styling/PomodoroTasks";
 
 function PomodoroTasks(props){
     const [edit, setEdit] = useState(false);
     
     return(
         edit ? 
-            <View style={styles.item}>
-                <View style={styles.itemLeft}>
-                    <TouchableOpacity onPress={()=>{setEdit(!edit)}} style={styles.square}>
+            <View style={PomodoroTasksStyles.item}>
+                <View style={PomodoroTasksStyles.itemLeft}>
+                    <TouchableOpacity onPress={()=>{setEdit(!edit)}} style={PomodoroTasksStyles.square}>
                         <Image source ={require('../resources/images/Checkmark2.png')}/>
                     </TouchableOpacity>
-                    <TextInput style={styles.itemText} editable={edit}>{props.text}</TextInput>
+                    <TextInput style={PomodoroTasksStyles.itemText} editable={edit}>{props.text}</TextInput>
                 </View>
             </View>
         : 
-            <View style={styles.item}>
-                <View style={styles.itemLeft}>
-                    <TouchableOpacity onPress={()=>{setEdit(!edit)}} style={styles.square}>
+            <View style={PomodoroTasksStyles.item}>
+                <View style={PomodoroTasksStyles.itemLeft}>
+                    <TouchableOpacity onPress={()=>{setEdit(!edit)}} style={PomodoroTasksStyles.square}>
                         <Image source ={require('../resources/images/PencilEdit.png')}/>
                     </TouchableOpacity>
-                    <TextInput style={styles.itemText} editable={edit}>{props.text}</TextInput>
+                    <TextInput style={PomodoroTasksStyles.itemText} editable={edit}>{props.text}</TextInput>
                 </View>
             </View>
     )
 }
 
-const styles = StyleSheet.create({
-    item:{
-        backgroundColor: '#D7F2BA',
-        padding: 77,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 16,
-        marginHorizontal: 33,
-    },
-    itemLeft:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        flexWrap: 'wrap'
-    },
-    square:{
-        width: 22,
-        height: 22,
-        borderRadius: 5,
-        opacity:0.4,
-        marginRight: 15,
-        top: -53,
-        left:186,
-        position:'absolute',
-    },
-    itemText:{
-        position: 'absolute',
-        width: 259,
-        height: 185,
-        top: -140,
-        left: -50,
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#6E8F6D',
-        fontStyle: 'normal',
-        lineHeight: 21,
-        maxWidth: '80%',
-    },
-});
 export default PomodoroTasks;
