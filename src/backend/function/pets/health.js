0420f68a9b7db291c7f0a1874d31832cf7fb0dbe
Darren Liu <darren.mega@gmail.com>
2022-01-29 20:35:05 -0500

    PET-40 Health Loss (#124)
    * feat: added habit DAO and re-implemented function to calculate habit's number of missed times
    
    * docs: added documentation for habit dao functions
    
    * chore: cleaned code and indentations
    
    * chore: fixed indentations
    
    * Added health loss methods and integrated health loss to trigger on missed habit.
    
    * Removed res object from internal functions.
    
    * Missing habits will now result in health loss.
    
    * fix: calculating habits missed times by day not by frequency
    
    * Removed extra logging statements. Reverted a deletion to fix health loss.
    
    Co-authored-by: Cheryl <cheryl.pl.chen@hotmail.com>

--- src/backend/function/pets/health.js
+++ src/backend/function/pets/health.js
@@ -2,52 +2,66 @@ const Pets = require('../../schemas/petsSchema');
 const User = require('../../schemas/userSchema');
 
 const { validationResult } = require('express-validator');
+const missedHabitPunishment = -10; // Health lost per time habit is missed.
+
 
 const addHealth = async (req, res) => {
-	try {
-		let errors = validationResult(req);
-		if (!errors.isEmpty())
-			return res.status(400).json({ error: errors.array() });
-
-		let user = await User.findById(req.user.id).select('-password');
-		if (!user) return res.status(404).json('User could not found');
-
-		let currentPet = await Pets.findOne({ user: req.user.id }).currentPet;
-		currentPet.hp = currentPet.hp + req.hp;
-		if (currentPet.hp > 100) {
-			currentPet.hp = 100;
-		}
-		await currentPet.save();
-
-		res.json(currentPet);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json('Server error');
-	}
-};
+	ret = modifyHealth(req.user.id, req.body.hp, res);
+	res.status(ret.status).json(ret.message);
+}
 
 const loseHealth = async (req, res) => {
+	ret = modifyHealth(req.user.id, -req.body.hp, res);
+	res.status(ret.status).json(ret.message);
+};
+
+const missedStreaksHealthLoss = async (id, numHabitsMissed, ) => {
 	try {
-		let errors = validationResult(req);
-		if (!errors.isEmpty())
-			return res.status(400).json({ error: errors.array() });
+		return modifyHealth(id, missedHabitPunishment * numHabitsMissed);
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+}
 
-		let user = await User.findById(req.user.id).select('-password');
-		if (!user) return res.status(404).json('User could not found');
+/*
+	Change health value by 'healthChange'.
+*/
+const modifyHealth = async (id, healthChange) => {
+	try {
+		// Find user
+		let user = await User.findById(id).select('-password');
+		if (!user) return {"status": 404,
+						   "message": "User could not be found."}
 
-		let currentPet = await Pets.findOne({ user: req.user.id }).currentPet;
-		currentPet.hp = currentPet.hp - req.hp;
+		// Find associated pet
+		let petQuery = await Pets.findOne({ user: id })
+		if (!petQuery) return {"status": 404,
+						       "message": "Pet could not found."}
+		currentPet = petQuery.currentPet;
+		currentPet.hp = currentPet.hp + healthChange;
+		
+		// Ensure 0 <= health <= 100
 		if (currentPet.hp < 0) {
 			currentPet.hp = 0;
 		}
-		await currentPet.save();
+		if (currentPet.hp > 100) {
+			currentPet.hp = 100;
+		}
+		
+		// Update pet health
+		await petQuery.save();
+		return {"status": 200,
+				"message": `Updated pet health by ${healthChange}`,
+				"currentPet": currentPet}
 
-		res.json(currentPet);
 	} catch (error) {
 		console.error(error);
-		res.status(500).json('Server error');
+		return {"status": 500,
+				"message": "Server Error"}
 	}
-};
+}
 
 exports.addHealth = addHealth;
 exports.loseHealth = loseHealth;
+exports.missedStreaksHealthLoss = missedStreaksHealthLoss;
